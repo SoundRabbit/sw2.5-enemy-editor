4fbe365e305f212b04f90a241ee4b4d0ad9a85f3
neunyan <soundrabbitstd@gmail.com>
2019-12-11 22:37:09 +0900

    編集用UIを作成

--- src/editor.rs
+++ src/editor.rs
@@ -4,17 +4,23 @@ use enemy::Enemy;
 use kagura::prelude::*;
 
 pub fn render(enemy: &Enemy) -> Html<Msg> {
+    let mut content = vec![
+        render_name(&enemy.name),
+        render_level_kind(&enemy.level, &enemy.kind),
+        render_props(&enemy),
+    ];
+    for part in &enemy.parts {
+        content.push(render_part(part));
+    }
+    content.push(render_append_part_button());
+    content.push(render_special_ability(&enemy.special_ability));
     Html::div(
         Attributes::new()
             .class("pure-form")
             .id("content")
             .string("data-type", "editor"),
         Events::new(),
-        vec![
-            render_name(&enemy.name),
-            render_level_kind(&enemy.level, &enemy.kind),
-            render_props(&enemy),
-        ],
+        content,
     )
 }
 
@@ -158,4 +164,87 @@ fn render_props(enemy: &Enemy) -> Html<Msg> {
             ),
         ],
     )
+}
+
+fn render_part(part: &enemy::Part) -> Html<Msg> {
+    Html::div(
+        Attributes::new(),
+        Events::new(),
+        vec![
+            Html::div(
+                Attributes::new().class("pure-control-group"),
+                Events::new(),
+                vec![
+                    Html::label(
+                        Attributes::new(),
+                        Events::new(),
+                        vec![Html::text("攻撃方法")],
+                    ),
+                    Html::input(Attributes::new().value(""), Events::new(), vec![]),
+                ],
+            ),
+            Html::div(
+                Attributes::new().class("pure-control-group"),
+                Events::new(),
+                vec![
+                    Html::label(Attributes::new(), Events::new(), vec![Html::text("命中")]),
+                    Html::input(Attributes::new().value(""), Events::new(), vec![]),
+                ],
+            ),
+            Html::div(
+                Attributes::new().class("pure-control-group"),
+                Events::new(),
+                vec![
+                    Html::label(Attributes::new(), Events::new(), vec![Html::text("打撃点")]),
+                    Html::input(Attributes::new().value(""), Events::new(), vec![]),
+                ],
+            ),
+            Html::div(
+                Attributes::new().class("pure-control-group"),
+                Events::new(),
+                vec![
+                    Html::label(Attributes::new(), Events::new(), vec![Html::text("回避力")]),
+                    Html::input(Attributes::new().value(""), Events::new(), vec![]),
+                ],
+            ),
+            Html::div(
+                Attributes::new().class("pure-control-group"),
+                Events::new(),
+                vec![
+                    Html::label(Attributes::new(), Events::new(), vec![Html::text("防護点")]),
+                    Html::input(Attributes::new().value(""), Events::new(), vec![]),
+                ],
+            ),
+            Html::div(
+                Attributes::new().class("pure-control-group"),
+                Events::new(),
+                vec![
+                    Html::label(Attributes::new(), Events::new(), vec![Html::text("HP")]),
+                    Html::input(Attributes::new().value(""), Events::new(), vec![]),
+                ],
+            ),
+            Html::div(
+                Attributes::new().class("pure-control-group"),
+                Events::new(),
+                vec![
+                    Html::label(Attributes::new(), Events::new(), vec![Html::text("MP")]),
+                    Html::input(Attributes::new().value(""), Events::new(), vec![]),
+                ],
+            ),
+        ],
+    )
+}
+
+fn render_append_part_button() -> Html<Msg> {
+    Html::button(
+        Attributes::new()
+            .class("pure-button")
+            .class("pure-button-primary"),
+        Events::new(),
+        vec![Html::text("部位を追加")],
+    )
+}
+
+fn render_special_ability(special_ability: &String) -> Html<Msg> {
+    Html::textarea(Attributes::new(), Events::new(), vec![])
 }
