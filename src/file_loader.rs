9b28276353c5f6bc8888338742f2c523dcf7ca32
neunyan <soundrabbitstd@gmail.com>
2019-12-12 18:31:09 +0900

    D&Dでファイル追加

--- src/file_loader.rs
+++ src/file_loader.rs
@@ -1,5 +1,6 @@
 use crate::Msg;
 use kagura::prelude::*;
+use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
 pub fn render() -> Html<Msg> {
@@ -16,11 +17,13 @@ pub fn render() -> Html<Msg> {
 fn render_drop_area() -> Html<Msg> {
     Html::div(
         Attributes::new().class("drop-area"),
-        Events::new(),
+        Events::new()
+            .on("dragover", on_dragover)
+            .on("drop", on_drop_file),
         vec![Html::span(
             Attributes::new(),
             Events::new(),
-            vec![Html::text("ここにファイルをドロップ（未対応）")],
+            vec![Html::text("ここにファイルをドロップ")],
         )],
     )
 }
@@ -48,6 +51,26 @@ fn on_select_file(e: web_sys::Event) -> Msg {
     if let Some(target) = e.target() {
         if let Ok(input) = target.dyn_into::<web_sys::HtmlInputElement>() {
             if let Some(file_list) = input.files() {
+                if let Some(file) = file_list.item(0) {
+                    return Msg::Load(file);
+                }
+            }
+        }
+    }
+    Msg::NoOp
+}
+
+fn on_dragover(e: web_sys::Event) -> Msg {
+    e.prevent_default();
+    Msg::NoOp
+}
+
+fn on_drop_file(e: web_sys::Event) -> Msg {
+    e.prevent_default();
+    web_sys::console::log_1(&JsValue::from(String::from("dropped!")));
+    if let Ok(e) = e.dyn_into::<web_sys::DragEvent>() {
+        if let Some(data) = e.data_transfer() {
+            if let Some(file_list) = data.files() {
                 if let Some(file) = file_list.item(0) {
                     return Msg::Load(file);
                 }
