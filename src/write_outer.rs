09cc50f55059f4aa4f276f4ea0227bf6e449149f
neunyan <soundrabbitstd@gmail.com>
2019-12-12 19:23:58 +0900

    書き出しのフロントデザインを作成

--- src/write_outer.rs
+++ src/write_outer.rs
@@ -8,8 +8,37 @@ pub fn render() -> Html<Msg> {
         Attributes::new()
             .class("pure-form")
             .id("content")
-            .string("data-type", "write-out"),
+            .string("data-type", "write-outer"),
         Events::new(),
-        vec![],
+        vec![render_option(
+            "Udonarium用に書き出し",
+            "Udonarium用のXMLファイルを書き出します。zip圧縮して利用してください。",
+            || Msg::NoOp,
+        )],
+    )
+}
+
+fn render_option(
+    name: impl Into<String>,
+    description: impl Into<String>,
+    handler: impl FnOnce() -> Msg + 'static,
+) -> Html<Msg> {
+    Html::div(
+        Attributes::new(),
+        Events::new(),
+        vec![
+            Html::button(
+                Attributes::new()
+                    .class("pure-button")
+                    .class("pure-button-primary"),
+                Events::new().on_click(|_| handler()),
+                vec![Html::text(name)],
+            ),
+            Html::span(
+                Attributes::new(),
+                Events::new(),
+                vec![Html::text(description)],
+            ),
+        ],
     )
 }
