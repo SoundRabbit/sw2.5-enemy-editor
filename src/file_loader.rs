1afd6ba4c0260dfc157c355ea12ce5172d31a1bc
neunyan <soundrabbitstd@gmail.com>
2019-12-12 11:21:08 +0900

    ファイル選択UIを作成

--- src/file_loader.rs
+++ src/file_loader.rs
@@ -8,6 +8,33 @@ pub fn render() -> Html<Msg> {
             .id("content")
             .string("data-type", "file-loader"),
         Events::new(),
-        vec![],
+        vec![render_drop_area(), render_file_selecter()],
+    )
+}
+
+fn render_drop_area() -> Html<Msg> {
+    Html::div(
+        Attributes::new().class("drop-area"),
+        Events::new(),
+        vec![Html::span(
+            Attributes::new(),
+            Events::new(),
+            vec![Html::text("ここにファイルをドロップ")],
+        )],
+    )
+}
+
+fn render_file_selecter() -> Html<Msg> {
+    Html::div(
+        Attributes::new().class("pure-form").class("file-selecter"),
+        Events::new(),
+        vec![
+            Html::label(
+                Attributes::new(),
+                Events::new(),
+                vec![Html::text("または：")],
+            ),
+            Html::input(Attributes::new().type_("file"), Events::new(), vec![]),
+        ],
     )
 }
